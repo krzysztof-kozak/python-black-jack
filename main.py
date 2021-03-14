c6d92bac802a8c9f48a2904b8da43009e0187881
Kris <hithard5@gmail.com>
2021-03-14 22:27:24 +0000

    added displaying hands and calculating scores

--- main.py
+++ main.py
@@ -11,19 +11,29 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
+from replit import clear
 from random import shuffle
+from functools import reduce
+
 from functions.starting_hand import draw_starting_hands
 
 deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 dealer_hand = []
 player_hand = []
 
-shuffle(deck)
-draw_starting_hands(dealer_hand, player_hand, deck)
+if input("Welcome to Black Jack\nBegin? (typ y or n) ").lower() == "y":
 
+	shuffle(deck)
+	draw_starting_hands(dealer_hand, player_hand, deck)
 
-print(dealer_hand)
-print(player_hand)
+	clear()
+
+	dealer_score = reduce(lambda a, b: a + b, dealer_hand)
+	player_score = reduce(lambda a, b: a + b, player_hand)
+
+	print(f"dealer's hand: {dealer_hand} | score = {dealer_score}")
+	print(f"player's hand: {player_hand} | score = {player_score}")
+
 
 
 
