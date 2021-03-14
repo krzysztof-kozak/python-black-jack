653d12b0c8e301ab44d60e3d13590057194ebf8c
Kris <hithard5@gmail.com>
2021-03-14 22:58:05 +0000

    added basic game flow, added stand option, added declare_winner function

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 ############### Our Blackjack House Rules #####################
 
-## The deck is unlimited in size. 
-## There are no jokers. 
+## The deck is unlimited in size.
+## There are no jokers.
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
 ## Use the following list as the deck of cards:
@@ -12,7 +12,7 @@
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 from replit import clear
-from random import shuffle
+from random import shuffle, choice
 from functools import reduce
 
 from functions.starting_hand import draw_starting_hands
@@ -21,21 +21,38 @@ deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 dealer_hand = []
 player_hand = []
 
-if input("Welcome to Black Jack\nBegin? (typ y or n) ").lower() == "y":
 
-	shuffle(deck)
-	draw_starting_hands(dealer_hand, player_hand, deck)
+def declare_winner(player_score, dealer_score):
+    if player_score > 21:
+        return f"BUST. You lose\nYour score was {player_score}\nDealer score was {dealer_score}"
 
-	clear()
+    while dealer_score < 17:
+        dealer_score += choice(deck)
 
-	dealer_score = reduce(lambda a, b: a + b, dealer_hand)
-	player_score = reduce(lambda a, b: a + b, player_hand)
+    if player_score > dealer_score:
+        return f"You won!\nYour score was {player_score}\nDealer score was {dealer_score}"
 
-	print(f"dealer's hand: {dealer_hand} | score = {dealer_score}")
-	print(f"player's hand: {player_hand} | score = {player_score}")
-
-
-
+    else:
+        return f"Dealer won!\nDealer score was {dealer_score}\nYour score was {player_score}"
 
 
+if input("Welcome to Black Jack\nBegin? (type y or n) ").lower() == "y":
 
+    shuffle(deck)
+    draw_starting_hands(dealer_hand, player_hand, deck)
+
+    clear()
+
+    dealer_score = reduce(lambda a, b: a + b, dealer_hand)
+    player_score = reduce(lambda a, b: a + b, player_hand)
+
+    print(f"Dealer's hand: { [dealer_hand[0], 'concealed'] }")
+    print("Dealer score: unknown\n")
+
+    print(f"Your hand: {player_hand}")
+    print(f"Your score: {player_score}")
+
+    if input("\nHit or stand? (type h or s) ") == "s":
+        winner = declare_winner(player_score, dealer_score)
+        clear()
+        print(winner)
